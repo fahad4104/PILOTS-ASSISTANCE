fd51ca523cbb886f17efe79f04567a865192a424
fahad4104 <uaeguy28@gmail.com>
2025-12-28 00:22:46 +0400

    Reset migrations for Postgres + init schema

--- prisma/migrations/20251227135536_add_manuals_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Manual" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "originalName" TEXT NOT NULL,
-    "blobUrl" TEXT NOT NULL,
-    "blobPathname" TEXT NOT NULL,
-    "openaiFileId" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
